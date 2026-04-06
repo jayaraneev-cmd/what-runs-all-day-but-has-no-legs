8be5cb1db80a9bbddb0e30a706501e617db2187d
jayara <jayaraneev@gmail.com>
2026-04-05 22:41:48 -0500

    Fix gradebook implementation and tests

--- main.cpp
+++ main.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <thread>
+#include "Timer.hpp"
 
-#include "src/timer.hpp"
+using namespace std;
 
 int main() {
-  // Your driver program goes here
+    Timer timer;
+
+    // Test 1: elapsed before start
+    cout << "Elapsed before start: " << timer.elapsed() << " seconds" << endl;
+
+    // Test 2: stop before start
+    timer.stop();
+    cout << "Called stop() before start (no crash)" << endl;
+
+    // Test 3: normal timing
+    cout << "\nStarting timer..." << endl;
+    timer.start();
+
+    this_thread::sleep_for(chrono::seconds(2));
+
+    cout << "Elapsed while running: " << timer.elapsed() << " seconds" << endl;
+
+    timer.stop();
+    cout << "Timer stopped." << endl;
+
+    cout << "Final elapsed time: " << timer.elapsed() << " seconds" << endl;
+
+    // Test 4: restart timer
+    cout << "\nRestarting timer..." << endl;
+    timer.start();
+
+    this_thread::sleep_for(chrono::seconds(1));
+
+    timer.stop();
+    cout << "Elapsed after restart: " << timer.elapsed() << " seconds" << endl;
+
+    return 0;
 }
