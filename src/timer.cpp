f7176d1b2f17649bc79fad9c0651d5dd45a2b672
jayara <jayaraneev@gmail.com>
2026-04-05 22:41:48 -0500

    Fix gradebook implementation and tests

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,5 +1,32 @@
-#include <string>
+#include "Timer.hpp"
 
-std::string pretty_print() {
-   return "Implement your code here!";
+Timer::Timer() {
+    running = false;
+    started = false;
+}
+
+void Timer::start() {
+    startTime = std::chrono::steady_clock::now();
+    running = true;
+    started = true;
+}
+
+void Timer::stop() {
+    if (!started || !running) return;
+
+    endTime = std::chrono::steady_clock::now();
+    running = false;
+}
+
+long long Timer::elapsed() {
+    if (!started) {
+        return 0;
+    }
+
+    if (running) {
+        auto now = std::chrono::steady_clock::now();
+        return std::chrono::duration_cast<std::chrono::seconds>(now - startTime).count();
+    } else {
+        return std::chrono::duration_cast<std::chrono::seconds>(endTime - startTime).count();
+    }
 }
--- src/timer.hpp
+++ src/timer.hpp
@@ -1,3 +1,21 @@
-#include <string>
+#ifndef TIMER_HPP
+#define TIMER_HPP
 
-std::string pretty_print();
+#include <chrono>
+
+class Timer {
+private:
+    std::chrono::time_point<std::chrono::steady_clock> startTime;
+    std::chrono::time_point<std::chrono::steady_clock> endTime;
+    bool running;
+    bool started;
+
+public:
+    Timer();
+
+    void start();
+    void stop();
+    long long elapsed(); // returns seconds
+};
+
+#endif
